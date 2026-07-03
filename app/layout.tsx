7231e67a45874c92e68f14ad7f2a1c81b545ec6b
Pham Dang Khoa <phamdangkhoa1806051076@gmail.com>
2026-07-03 14:10:58 +0700

    design(typography): swap Inter for Plus Jakarta Sans, upgrade type hierarchy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import { Inter, Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { PlayerProvider } from "@/context/PlayerContext";
@@ -7,14 +7,18 @@ import { AuthProvider } from "@/context/AuthContext";
 import { LanguageProvider } from "@/context/LanguageContext";
 import Background from "@/components/Background";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
-
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
   variable: "--font-plus-jakarta",
+  display: "swap",
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-jetbrains",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -38,21 +42,12 @@ export default function RootLayout({
     <html lang="vi" suppressHydrationWarning>
       <head>
         <link 
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link 
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Plus+Jakarta+Sans:wght@400;500;600;700;800&family=JetBrains+Mono:wght@600&display=swap"
-        />
-        <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
           crossOrigin="anonymous"
         />
       </head>
-      <body className={`${inter.variable} ${plusJakarta.variable}`}>
+      <body className={`${plusJakarta.variable} ${jetbrainsMono.variable}`}>
         <AuthProvider>
           <LanguageProvider>
             <ThemeProvider>
